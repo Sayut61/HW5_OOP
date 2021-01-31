1c4a3d9270b37ec39645048a055eacf2de6559bc
Anton Sayutin <sayutosss@gmail.com>
2021-01-31 14:40:58 +0300

    task4

--- src/Tasc4/EthernetAdapter.java
+++ src/Tasc4/EthernetAdapter.java
@@ -1,5 +1,7 @@
 package Tasc4;
 
+import java.util.Objects;
+
 public class EthernetAdapter extends Device {
     int speed;
     String mac;
@@ -9,6 +11,32 @@ public class EthernetAdapter extends Device {
         this.speed = speed;
         this.mac = mac;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj != null && obj.getClass() == this.getClass()){
+            EthernetAdapter device = (EthernetAdapter) obj;
+            boolean res1 = device.manufacturer == this.manufacturer;
+            boolean res2 = device.price == this.price;
+            boolean res3 = device.serialNumber == this.serialNumber;
+            boolean res4 = device.speed == this.speed;
+            boolean res5 = device.mac == this.mac;
+            return res1 && res2 && res3 && res4 && res5;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 37;
+        result = 37 * result + (manufacturer != null ? manufacturer.hashCode() : 0);
+        result = 37 * result + Float.floatToRawIntBits(price);
+        result = 37 * result + (serialNumber != null ? serialNumber.hashCode() : 0);
+        result = 37 * result + speed;
+        result = 37 * result + (mac != null ? mac.hashCode() : 0);
+        return result;
+    }
+
 
     public int getSpeed() {
         return speed;
--- src/Tasc4/Main.java
+++ src/Tasc4/Main.java
@@ -2,6 +2,16 @@ package Tasc4;
 
 public class Main {
     public static void main(String[] args) {
+        Device device = new Device("Samsung", 120, "AB1234567CD");
+        Monitor monitor = new Monitor("Samsung", 120, "AB1234567CD", 1280, 1024 );
+        EthernetAdapter ethernetAdapter = new EthernetAdapter("TP-Link", 120, "12341", 100, "211314");
+        EthernetAdapter ethernetAdapter1 = new EthernetAdapter("TP-Link", 120, "12341", 100, "211314");
+        System.out.println(device.equals(monitor));
+        System.out.println(device.hashCode());
+        System.out.println(monitor.hashCode());
+        System.out.println(ethernetAdapter.equals(ethernetAdapter1));
+        System.out.println(ethernetAdapter.hashCode());
+        System.out.println(ethernetAdapter1.hashCode());
 
     }
 }
